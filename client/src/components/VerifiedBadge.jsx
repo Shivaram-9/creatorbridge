c87bd998fb609424d5adf24935960e66fda0aa30
Shivaram-9 <ymvshiva1784@gmail.com>
2026-07-06 08:23:51 +0530

    Standardize badge size to fixed 18px everywhere

--- client/src/components/VerifiedBadge.jsx
+++ client/src/components/VerifiedBadge.jsx
@@ -32,8 +32,8 @@ export default function VerifiedBadge({ role = 'influencer', style = {}, classNa
         loading="lazy"
         decoding="async" 
         style={{ 
-          width: '1em', 
-          height: '1em', 
+          width: '18px', 
+          height: '18px', 
           display: 'block', 
           objectFit: 'contain',
           pointerEvents: 'none'
